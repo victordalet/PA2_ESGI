19884a17d78c3012e732a809f021099566d28489
victor <victor.dalet>
2024-03-11 21:15:31 +0100

    feat(mobile-app): Add front login

--- mobile-app/app/app/src/main/java/com/example/app/MainActivity.kt
+++ mobile-app/app/app/src/main/java/com/example/app/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.app
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -10,40 +10,34 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var emailValue: EditText
-    private lateinit var passwordValue: EditText
-    private lateinit var button: Button
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var loginButton: Button
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        this.emailValue = findViewById(R.id.editTextTextEmailAddress)
-        this.passwordValue = findViewById(R.id.editTextNumberPassword)
-        this.button = findViewById(R.id.buttonSign)
 
-        button.setOnClickListener() {
-            val email = emailValue.text.toString()
-            val password = passwordValue.text.toString()
-            testConnection(email, password)
-        }
+        this.password = findViewById(R.id.password)
+        this.email = findViewById(R.id.email)
+        this.loginButton = findViewById(R.id.loginButton)
+        this.loginButton.setOnClickListener(View.OnClickListener {
+            val apiPath = "http://127.0.0.1:3001/api/v1/auth/sign_in"
+            try {
+                val url = URL(apiPath)
+                with(url.openConnection() as HttpURLConnection) {
+                    requestMethod = "POST"
+                    setRequestProperty("Content-Type", "application/json; utf-8")
+                    setRequestProperty("Accept", "application/json")
+                    doOutput = true
+                    val jsonInputString = "{\"email\": \"$email\", \"password\": \"$password\"}"
+                    outputStream.write(jsonInputString.toByteArray(Charsets.UTF_8))
+                    println("Response code: $responseCode")
+                    println("Response message: $responseMessage")
+                }
+            } catch (e: Exception) {
+                println(e)
+            }
+        })
     }
-
-    private fun testConnection(email: String, password: String) {
-        val apiPath = "http://localhost:3000/api/v1/auth/sign_in"
-        val url = URL(apiPath)
-        with(url.openConnection() as HttpURLConnection) {
-            requestMethod = "POST"
-            setRequestProperty("Content-Type", "application/json; utf-8")
-            setRequestProperty("Accept", "application/json")
-            doOutput = true
-            val jsonInputString = "{\"email\": \"$email\", \"password\": \"$password\"}"
-            outputStream.write(jsonInputString.toByteArray(Charsets.UTF_8))
-            println("Response code: $responseCode")
-            println("Response message: $responseMessage")
-        }
-
-
-    }
-
 }
