5d7006193bb5324bed4d7205f38ac11698144d8c
victor <victor.dalet>
2024-03-17 19:05:22 +0100

    fix(map): FIx cursor position with geocode

--- front-client/src/components/map.tsx
+++ front-client/src/components/map.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 export interface Coordinate {
-    lat: number;
-    long: number
+    lat?: number;
+    long?: number
     city: string;
 }
 
@@ -24,15 +24,18 @@ export class MapComponents extends React.Component<{ dataCoordinate: Coordinate[
                 overflow: 'hidden',
                 border: '2px solid #000'
             }}>
-                <MapContainer center={[48.6, 2.34]} zoom={10} scrollWheelZoom={false}>
+                <MapContainer center={[44.6, 2.34]} zoom={6} scrollWheelZoom={false}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                     {
-                        dataCoordinate.map((c, index) => {
+                        dataCoordinate.map( (c, index) => {
+                            if (c.lat === undefined || c.long === undefined) {
+                                return;
+                            }
                             return (
-                                <Marker key={index} position={[c.lat, c.long]}>
+                                <Marker key={index} position={[c.lat , c.long]}>
                                     <Popup>
                                         {c.city}
                                     </Popup>
