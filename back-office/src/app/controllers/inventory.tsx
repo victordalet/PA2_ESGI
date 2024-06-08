0c55e728c1ae72842b20c7ee6a00656ff57a940b
victordalet <victor.dalet@protonmail.com>
2024-06-08 18:15:44 +0200

    feat(back-office): Inventory PDF

--- back-office/src/app/controllers/inventory.tsx
+++ back-office/src/app/controllers/inventory.tsx
@@ -1,4 +1,4 @@
-import {ControllerProps, ControllerState} from "../@types/inventory";
+import {ControllerProps, ControllerState, Inventory} from "../@types/inventory";
 import React from "react";
 import {InventoryView} from "../views/inventory";
 import {InventoryModel} from "../model/inventory";
@@ -18,7 +18,8 @@ export default class InventoryController extends React.Component<ControllerProps
     }
 
     state: ControllerState = {
-        inventory: []
+        inventory: [],
+        inventoryState: []
     };
 
     private getInventory = async () => {
@@ -27,9 +28,31 @@ export default class InventoryController extends React.Component<ControllerProps
         this.setState({inventory});
     };
 
+    public addInventory = async () => {
+        const name = document.querySelector('input[placeholder="Material"]') as HTMLInputElement;
+        const state = document.querySelector('input[placeholder="State"]') as HTMLInputElement;
+        const description = document.querySelector('input[placeholder="Description"]') as HTMLInputElement;
+        const inventory = {
+            name: name.value,
+            state: state.value,
+            description: description.value
+        };
+        this.setState({inventoryState: [...this.state.inventoryState, inventory]});
+        name.value = '';
+        state.value = '';
+        description.value = '';
+    };
+
+    public generatePDF = async () => {
+        await this.inventoryViewModel.generatePDF(this.state.inventoryState);
+    };
+
     render() {
         return (
             <InventoryView
+                generatePDF={this.generatePDF}
+                addInventory={this.addInventory}
+                inventoryState={this.state.inventoryState}
                 inventory={this.state.inventory}
             />
         );
--- back-office/src/app/views/inventory.tsx
+++ back-office/src/app/views/inventory.tsx
@@ -10,7 +10,12 @@ export class InventoryView extends React.Component<ViewProps> {
 
     render() {
 
-        const {inventory} = this.props;
+        const {
+            inventory,
+            inventoryState,
+            addInventory,
+            generatePDF
+        } = this.props;
         const now = momentLocalizer(moment);
 
         return (
@@ -40,6 +45,23 @@ export class InventoryView extends React.Component<ViewProps> {
                                 city: `${i.user_email}-${new Date(i.state_place).toLocaleDateString()}`
                             };
                         })}/>
+                    </div>
+                    <div className={"form-inventory"}>
+                        <h2>Add Inventory</h2>
+                        <select>
+                            {
+                                inventory.map(i => {
+                                    return <option value={i.user_email}>{i.user_email}</option>;
+                                })
+                            }
+                        </select>
+                        <div className={"form-inventory__input"}>
+                            <input type="text" placeholder={"Material"}/>
+                            <input type="text" placeholder={"State"}/>
+                            <input type="text" placeholder={"Description"}/>
+                            <button onClick={addInventory}>Add</button>
+                        </div>
+                        <button onClick={generatePDF}>Generate PDF</button>
                     </div>
                 </div>
             </div>
